1c09321dade9be89cb7d583a7cc46d4c972c0014
Paul Ruiz <paul.ruiz@workday.com>
2019-02-15 07:25:25 -0800

    Tutorial 2 code - location tracking

--- AndroidMaps/app/src/main/java/com/ptrprograms/androidmaps/MapsActivity.kt
+++ AndroidMaps/app/src/main/java/com/ptrprograms/androidmaps/MapsActivity.kt
@@ -1,34 +1,129 @@
 package com.ptrprograms.androidmaps
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
+    GoogleMap.OnMyLocationButtonClickListener,
+    GoogleMap.OnMyLocationClickListener {
 
     private lateinit var mMap: GoogleMap
+
+    private val LOCATION_PERMISSION = 42
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            initMap()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION)
+        }
+    }
 
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun initMap() {
+        mMap.setMyLocationEnabled(true)
+        mMap.setOnMyLocationButtonClickListener(this)
+        mMap.setOnMyLocationClickListener(this)
+
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                updateMapLocation(location)
+            }
+
+        initLocationTracking()
+
+    }
+
+    private fun initLocationTracking() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations){
+                    updateMapLocation(location)
+                }
+            }
+        }
+
+        fusedLocationClient.requestLocationUpdates(
+            LocationRequest(),
+            locationCallback,
+            null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if( ::mMap.isInitialized ) {
+            initLocationTracking()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun updateMapLocation(location: Location?) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(
+            location?.latitude ?: 0.0,
+            location?.longitude ?: 0.0)))
+
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION) {
+            if (permissions.size == 1 &&
+                permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                initMap()
+            }
+        }
+    }
+
+    override fun onMyLocationButtonClick(): Boolean {
+        Toast.makeText(this, "my location button click", Toast.LENGTH_LONG).show()
+        return false
+    }
+
+    override fun onMyLocationClick(p0: Location) {
+        Toast.makeText(this, "my location click", Toast.LENGTH_LONG).show()
     }
 }
