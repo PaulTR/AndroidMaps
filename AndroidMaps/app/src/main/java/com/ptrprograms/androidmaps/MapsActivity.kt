1b4ac53b79a1c8f099b1ad75f427aaf516607ab5
Paul Ruiz <paul.ruiz@workday.com>
2019-02-18 12:51:06 -0700

    Tutorial 3: configuring views and tools

--- AndroidMaps/app/src/main/java/com/ptrprograms/androidmaps/MapsActivity.kt
+++ AndroidMaps/app/src/main/java/com/ptrprograms/androidmaps/MapsActivity.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -14,7 +16,9 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     GoogleMap.OnMyLocationButtonClickListener,
@@ -26,6 +30,19 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
+
+    private var compassEnabled = false
+    private var tiltEnabled = false
+    private var indoorLevelPickerEnabled = false
+    private var scrollEnabled = false
+    private var rotateEnabled = false
+    private var zoomEnabled = false
+    private var buildingsEnabled = false
+    private var boundariesEnabled = false
+    private var currentLocation: Location? = null
+
+    private val DENVER = LatLng(39.7392, -104.9903)
+    private val FRESNO = LatLng(36.7378, -119.7871)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,10 +69,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     }
 
     private fun initMap() {
-        mMap.setMyLocationEnabled(true)
+        mMap.isMyLocationEnabled = true
         mMap.setOnMyLocationButtonClickListener(this)
         mMap.setOnMyLocationClickListener(this)
-
 
         fusedLocationClient.lastLocation
             .addOnSuccessListener { location : Location? ->
@@ -95,11 +111,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
     }
 
     private fun updateMapLocation(location: Location?) {
+        currentLocation = location
         mMap.moveCamera(CameraUpdateFactory.newLatLng(LatLng(
             location?.latitude ?: 0.0,
             location?.longitude ?: 0.0)))
 
+        updateUiSettings()
         mMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f))
+    }
+
+    private fun updateUiSettings() {
+        mMap.isBuildingsEnabled = buildingsEnabled
+        mMap.uiSettings.isTiltGesturesEnabled = tiltEnabled
+        mMap.uiSettings.isCompassEnabled = compassEnabled
+        mMap.uiSettings.isIndoorLevelPickerEnabled = indoorLevelPickerEnabled
+        mMap.uiSettings.isRotateGesturesEnabled = rotateEnabled
+        mMap.uiSettings.isScrollGesturesEnabled = scrollEnabled
+        mMap.uiSettings.isZoomControlsEnabled = zoomEnabled
+        mMap.uiSettings.isZoomGesturesEnabled = zoomEnabled
+
+        if( boundariesEnabled && currentLocation != null ) {
+            val bounds = LatLngBounds(
+                LatLng(currentLocation!!.latitude - 0.001, currentLocation!!.longitude - 0.001),
+                LatLng(currentLocation!!.latitude + 0.001, currentLocation!!.longitude + 0.001))
+            mMap.setLatLngBoundsForCameraTarget(bounds)
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -125,5 +161,74 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
 
     override fun onMyLocationClick(p0: Location) {
         Toast.makeText(this, "my location click", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.map_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if( item?.isCheckable ?: false ) {
+            item!!.isChecked = !item.isChecked
+        }
+
+        when( item?.itemId ) {
+            R.id.compass -> {
+                compassEnabled = item.isChecked
+            }
+            R.id.tilt -> {
+                tiltEnabled = item.isChecked
+            }
+            R.id.indoor -> {
+                indoorLevelPickerEnabled = item.isChecked
+            }
+            R.id.scroll -> {
+                scrollEnabled = item.isChecked
+            }
+            R.id.rotate -> {
+                rotateEnabled = item.isChecked
+            }
+            R.id.zoom -> {
+                zoomEnabled = item.isChecked
+            }
+            R.id.buildings -> {
+                buildingsEnabled = item.isChecked
+            }
+            R.id.boundaries -> {
+                boundariesEnabled = item.isChecked
+            }
+        }
+
+        when( item?.itemId ) {
+            R.id.animate -> {
+                val cameraPositionFresno = CameraPosition.Builder()
+                    .bearing(0f)
+                    .zoom(15f)
+                    .target(FRESNO)
+                    .build()
+
+                val cameraPositionDenver = CameraPosition.Builder()
+                    .bearing(0f)
+                    .zoom(15f)
+                    .target(DENVER)
+                    .build()
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPositionFresno), 3000, object: GoogleMap.CancelableCallback {
+                    override fun onFinish() {
+                        Thread.sleep(3000)
+                        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPositionDenver), 3000, null)
+                    }
+
+                    override fun onCancel() { }
+                })
+
+            }
+            else -> {
+                updateUiSettings()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
